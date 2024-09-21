4745213f5b2a511e4af97b33ad9bcccbaffd531f
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-09-22 02:05:23 +0530

    Implement GET store data from firebase db.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import { db } from "@/firebase";
+import { Store } from "@/types-db";
+import { auth } from "@clerk/nextjs/server";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface SetupLayoutProp {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const SetupLayout = ({ children }: SetupLayoutProp) => {
-    return (
-        <div>{children}</div>
-    )
-}
+const SetupLayout = async ({ children }: SetupLayoutProp) => {
+  const { userId } = auth();
 
-export default SetupLayout
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const storeSnap = await getDocs(
+    query(collection(db, "stores"), where("userId", "==", userId))
+  );
+
+  let store = null as any;
+
+  storeSnap.forEach((doc) => {
+    store = doc.data() as Store;
+    return;
+  });
+
+  if (store) {
+    redirect(`/${store?.id}`);
+  }
+
+  return <div>{children}</div>;
+};
+
+export default SetupLayout;
