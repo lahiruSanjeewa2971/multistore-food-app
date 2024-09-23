6edbda1f7014e6fd89916eb6443606f8c069fa24
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-09-23 18:24:22 +0530

    Finished settings screen | update store name

--- app/(dashboard)/[storeId]/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/settings/components/settings-form.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { ApiAlert } from "@/components/api-alert";
 import { Heading } from "@/components/heading";
+import { AlertModal } from "@/components/modal/alert-modal";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { Store } from "@/types-db";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
 interface SettingsFormProps {
@@ -23,8 +37,10 @@ const formSchema = z.object({
 
 export const SettingsForm = ({ initialData }: SettingsFormProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -33,13 +49,52 @@ export const SettingsForm = ({ initialData }: SettingsFormProps) => {
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     console.log("onSubmit :", data);
+    try {
+      setIsLoading(true);
+
+      const response = await axios.patch(`/api/stores/${params.storeId}`, data);
+      console.log("submit | response :", response);
+      toast.success("Store updated.");
+      router.refresh();
+    } catch (error) {
+      console.log("error :", error);
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.delete(`/api/stores/${params.storeId}`);
+      toast.success("Store removed");
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log("error :", error);
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
   };
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={isLoading}
+      />
       <div className="flex items-center justify-center">
         <Heading title="Settings" description="Manage Store Preferences." />
-        <Button variant={"destructive"} size={"icon"} onClick={() => {}}>
+        <Button
+          variant={"destructive"}
+          size={"icon"}
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
@@ -47,6 +102,43 @@ export const SettingsForm = ({ initialData }: SettingsFormProps) => {
       <Separator />
 
       {/* update store name form */}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Your store name..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} type="submit" size={"sm"}>
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+
+      <Separator />
+
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        varient="public"
+      />
     </>
   );
 };
