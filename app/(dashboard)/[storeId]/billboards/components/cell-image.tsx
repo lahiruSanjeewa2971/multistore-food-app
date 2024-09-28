c2a7d180504e3297e8e245b64177a2cf69e69c9a
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-09-29 01:03:59 +0530

    Implement fetch Billboards data from firebase and display within a table

--- app/(dashboard)/[storeId]/billboards/components/cell-image.tsx
+++ app/(dashboard)/[storeId]/billboards/components/cell-image.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import Image from "next/image";
+
 interface CellImageProps {
   imageUrl: string;
 }
 
 export const CellImage = ({ imageUrl }: CellImageProps) => {
-  return <div>CellImage</div>;
+  return (
+    <div className="overflow-hidden w-32 h-16 min-w-32 min-h-16 relative rounded-md shadow-md">
+      <Image fill alt="Billboard Image" src={imageUrl} />
+    </div>
+  );
 };
