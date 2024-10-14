6c311522b811f5436b0de62b38ea6097fb48a653
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-10-14 22:06:20 +0530

    Start cuisines screens

---
+++ app/(dashboard)/[storeId]/cuisines/page.tsx
@@ -0,0 +1,31 @@
+import { db } from "@/firebase";
+import { Cuisine, Kitchen } from "@/types-db";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { CuisinesClient } from "./components/client";
+import { CuisinesColumns } from "./components/columns";
+import { format } from "date-fns";
+
+const CuisinesPage = async ({ params }: { params: { storeId: string } }) => {
+  const cuisineData = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "cuisines"))
+  ).docs.map((doc) => doc.data()) as Cuisine[];
+
+  const formattedCuisines: CuisinesColumns[] = cuisineData.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: item.createdAt
+      ? format(item.createdAt.toDate(), "MMMM do, yyyy")
+      : "",
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CuisinesClient data={formattedCuisines} />
+      </div>
+    </div>
+  );
+};
+
+export default CuisinesPage;
